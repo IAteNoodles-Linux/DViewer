f9da4e279b437ab2323f385fb24f77702172ceaa
IAteNoodles <iatenoodles.linux@protonmail.com>
2022-06-15 00:25:03 +0530

    Changed Database.py, instead of a static approach, a class based method is used. May lead to bugs, which needs to be fixed.

--- Python/CLI/Database.py
+++ Python/CLI/Database.py
@@ -1,54 +1,249 @@
 #!/bin/env python
 
-import sys
 import mariadb
-
+import sys
 # Variables
 
-def __init__(self, user: str, password: str, host: str, database: str):
+class SQL:
     """
-    Create a connection to a database.
-
-    Args:
-        user: Username.
-        password: Password.
-        host: Hostname.
-        database: Database name.
+    Class for working with SQL.
     """
-    self.current_table = None
-    try:
-        return mariadb.connect(host=host, user=user, password=password, database=database)
-    except mariadb.Error as e:
-        print("Error %d: %s" % (e.args[0], e.args[1]))
-        sys.exit(1)
+    def __init__(self, user: str, password: str, host: str, database: str):
+        """
+        Create a connection to a database.
 
-def send_command(self, command: str, commit=False, fetch=True):
-    """
-    Send a command to the database and return the result
+        Args:
+            user: Username.
+            password: Password.
+            host: Hostname.
+            database: Database name.
+        """
+        self.current_table = None
+        try:
+            self.connection = mariadb.connect(host=host, user=user, password=password, database=database)
+        except mariadb.Error as e:
+            print("Error %d: %s" % (e.args[0], e.args[1]))
 
-    Args:
-        self: Connection to the database.
-        command: Command to send to the database.
-        commit: Whether to commit the changes. Default is False.
-        fetch: Number of rows to fetch. Default is True, which means to fetch all rows.
+    def send_command(self, command: str, commit=False, fetch=True):
+        """
+        Send a command to the database and return the result
 
-    Returns:
-        tuple: Executes the command and fetches the result.
-    """
+        Args:
+            self: Connection to the database.
+            command: Command to send to the database.
+            commit: Whether to commit the changes. Default is False.
+            fetch: Number of rows to fetch. Default is True, which means to fetch all rows.
 
-    try:
-        cursor = self.cursor()
-        cursor.execute(command)
-        if commit:
-            self.commit()
-        if fetch:
-            return cursor.fetchall()
+        Returns:
+            tuple: Executes the command and fetches the result.
+            False, if the command failed.
+        """
+
+        try:
+            cursor = self.connection.cursor()
+            cursor.execute(command)
+            if commit:
+                self.connection.commit()
+            if fetch:
+                return cursor.fetchall()
+            else:
+                return cursor.fetchmany(fetch)
+        except mariadb.Error as e:
+            #@TODO: Handle errors and report appropriately.
+            print("Error %d: %s" % (e.args[0], e.args[1]))
+            sys.exit(1)
+
+    def show_databases(self):
+        """
+        Show all databases.
+        """
+        command = "SHOW DATABASES"
+        return self.send_command(command)
+
+    def add_database(self, database: str):
+        """
+        Add a database.
+
+        Args:
+            database: Name of the database to add.
+        """
+        command = "CREATE DATABASE %s" % database
+        return self.send_command(command)
+
+    def show_permissions_on_database(self, database: str):
+        """
+        Show the permissions on a database.
+
+        Args:
+            database: Name of the database to show the permissions on.
+        """
+        command = "SELECT * FROM mysql.db WHERE Db = '%s'" % database
+        return self.send_command(command)
+
+    def update_permissions_on_database(self, database: str, user: str, host: str, permissions: str):
+        """
+        Update the permissions on a database.
+
+        Args:
+            database: Name of the database to update the permissions on.
+            user: Username.
+            host: Hostname.
+            permissions: New permissions.
+        """
+
+        command = "GRANT %s ON %s.* TO '%s'@'%s'" % (permissions, database, user, host)
+        return self.send_command(command)
+
+    def switch_database(self, database: str):
+        """
+        Switch to a different database.
+        
+        Args:
+            database: Name of the database to switch to.
+        """
+        command = "USE %s" % database
+        return self.send_command(command)
+
+    def show_tables(self, *args):
+        """
+        Show all tables in the database.
+        """
+        if len(args) != 0:
+            command = "SHOW TABLES FROM %s" % args[0]
         else:
-            return cursor.fetchmany(fetch)
-    except mariadb.Error as e:
-        #@TODO: Handle errors and report appropriately.
-        print("Error %d: %s" % (e.args[0], e.args[1]))
-        sys.exit(1)
+            command = "SHOW TABLES"
+        return self.send_command(command)
+
+    def select_table(self, table_name: str):
+        """
+        Selects a table to work with.
+
+        Args:
+            table_name: Name of the table to select.
+        """
+        self.current_table = table_name
+
+    def show_columns(self, *args):
+        """
+        Get the columns and types of a table.
+
+        Args:
+            table_name: Name of the table to get the columns and types from. If not specified, the current table is used.
+
+        Returns:
+            dict: Dictionary of columns and their attributes.
+            {name: {column_type, is_nullable, key, default, extra}}
+
+        """
+        if len(args) == 0:
+            command = "SHOW COLUMNS FROM %s" % self.current_table
+        else:
+            command = "DESCRIBE %s" % args[0]
+        data = self.send_command(command)
+        columns = {}
+        for row in data:
+            columns[row[0]] = {
+                "column_type": row[1],
+                "is_nullable": row[2],
+                "key": row[3],
+                "default": row[4],
+                "extra": row[5]
+            }
+        return columns
+
+    def create_table(self,  table_name: str, columns_with_types: dict):
+        """
+        Create a table.
+
+        Args:
+            table_name: Name of the table to create.
+            columns_with_types: Dictionary of columns and their types.
+        """
+        command = "CREATE TABLE %s (%s)" % (table_name, ", ".join(["%s %s" % (key, columns_with_types[key]) for key in columns_with_types.keys()]))
+        return self.send_command(command)
+
+    def add_column(self, column_name: str, column_type: str, *args):
+        """
+        Add a column to a table.
+
+        Args:
+            column_name: Name of the column to add.
+            column_type: Type of the column to add.
+        """
+        if len(args) == 0:
+            table_name = self.current_table
+        else:
+            table_name = args[0]
+        command = "ALTER TABLE %s ADD COLUMN %s %s" % (table_name, column_name, column_type)
+        return self.send_command(command)
+
+    def insert_data(self, *args, **kwargs):
+        """
+        Insert data into a table.
+
+        Args:
+            table_name: Name of the table to insert data into. If not specified, the current table is used.
+            **kwargs: Keyword arguments. The key is the column name and the value is the data to insert.
+        """
+        if len(args) == 0:
+            table_name = self.current_table
+        else:
+            table_name = args[0]
+        command = "INSERT INTO %s (%s) VALUES (%s)" % (table_name, ", ".join(kwargs.keys()), ", ".join(["'%s'" % kwargs[key] for key in kwargs.keys()]))
+        return self.send_command(command)
+
+    def get_data(self, *data):
+        """
+        Get data from a table.
+
+        Args:
+            table_name: Name of the table to get data from.
+            columns: List of columns to get data from. If not specified, all columns are used.
+        """
+        if len(data) == 1:
+            command = "SELECT * FROM %s" % data[0]
+        else:
+            command = "SELECT %s FROM %s" % (", ".join(data[1:]), data[0])
+        return self.send_command(command)
+
+    def get_table_creation_script(self, *args):
+        """
+        Returns the creation script for a table.
+
+        Args:
+            table_name: Name of the table to show the creation script for.
+        """
+        if len(args) == 0:
+            command = "SHOW CREATE TABLE %s" % self.current_table
+        else:
+            command = "SHOW CREATE TABLE %s" % args[0]
+        return self.send_command(command)
+
+    def join_table(self, left: str, right: str, join_type: str, conditions: str):
+        """
+        Join two tables.
+
+        Args:
+            left: Name of the left table.
+            right: Name of the right table.
+            join_type: Type of join. Valid values are: INNER, LEFT, RIGHT, CROSS.
+            conditions: Conditions to join on.
+        """
+        command = "SELECT * FROM %s %s JOIN %s ON %s" % (left, join_type, right, conditions)
+        return self.send_command(command)
+
+    def get_foreign_keys(self, *args):
+        """
+        Get the foreign keys for a table.
+
+        Args:
+            table_name: Name of the table to get the foreign keys for.
+        """
+        if len(args) == 0:
+            command = "SELECT * FROM information_schema.key_column_usage WHERE REFERENCED_TABLE_NAME = '%s'" % self.current_table
+
+        command = "SELECT * FROM information_schema.KEY_COLUMN_USAGE WHERE TABLE_NAME = '%s' AND REFERENCED_TABLE_NAME IS NOT NULL" % args[0]
+        return self.send_command(command)
 
 def print__data_as_dictionary(key: list,data: tuple):
     """
@@ -65,172 +260,4 @@ def print__data_as_dictionary(key: list,data: tuple):
 
     for i in range(len(key)):
         print(key[i], data[i])
-
-def show_databases(self):
-    """
-    Show all databases.
-    """
-    command = "SHOW DATABASES"
-    return send_command(self.connection, command)
-
-def show_permissions_on_database(self, database: str):
-    """
-    Show the permissions on a database.
-
-    Args:
-        database: Name of the database to show the permissions on.
-    """
-    command = "SELECT * FROM mysql.db WHERE Db = '%s'" % database
-    return send_command(self.connection, command)
-
-def update_permissions_on_database(self, database: str, user: str, host: str, permissions: str):
-    """
-    Update the permissions on a database.
-
-    Args:
-        database: Name of the database to update the permissions on.
-        user: Username.
-        host: Hostname.
-        permissions: New permissions.
-    """
-
-    command = "GRANT %s ON %s.* TO '%s'@'%s'" % (permissions, database, user, host)
-    return send_command(self.connection, command)
-
-def switch_database(self, database: str):
-    """
-    Switch to a different database.
-    
-    Args:
-        database: Name of the database to switch to.
-    """
-    command = "USE %s" % database
-    return send_command(self.connection, command)
-
-def show_tables(self, *args):
-    """
-    Show all tables in the database.
-    """
-    if len(args) != 0:
-        command = "SHOW TABLES FROM %s" % args[0]
-    else:
-        command = "SHOW TABLES"
-    return send_command(self.connection, command)
-
-def select_table(self, table_name: str):
-    """
-    Selects a table to work with.
-
-    Args:
-        table_name: Name of the table to select.
-    """
-    self.current_table = table_name
-
-def show_columns(self, *args):
-    """
-    Get the columns and types of a table.
-
-    Args:
-        table_name: Name of the table to get the columns and types from. If not specified, the current table is used.
-
-    Returns:
-        dict: Dictionary of columns and their attributes.
-        {name: {column_type, is_nullable, key, default, extra}}
-
-    """
-    if len(args) == 0:
-        command = "SHOW COLUMNS FROM %s" % self.current_table
-    else:
-        command = "DESCRIBE %s" % args[0]
-    data = send_command(self.connection, command)
-    columns = {}
-    for row in data:
-        columns[row[0]] = {
-            "column_type": row[1],
-            "is_nullable": row[2],
-            "key": row[3],
-            "default": row[4],
-            "extra": row[5]
-        }
-    return columns
-
-def create_table(self,  table_name: str, columns_with_types: dict):
-    """
-    Create a table.
-
-    Args:
-        table_name: Name of the table to create.
-        columns_with_types: Dictionary of columns and their types.
-    """
-    command = "CREATE TABLE %s (%s)" % (table_name, ", ".join(["%s %s" % (key, columns_with_types[key]) for key in columns_with_types.keys()]))
-    return send_command(self.connection, command)
-
-def insert_data(self, *args, **kwargs):
-    """
-    Insert data into a table.
-
-    Args:
-        table_name: Name of the table to insert data into. If not specified, the current table is used.
-        **kwargs: Keyword arguments. The key is the column name and the value is the data to insert.
-    """
-    if len(args) == 0:
-        table_name = self.current_table
-    else:
-        table_name = args[0]
-    command = "INSERT INTO %s (%s) VALUES (%s)" % (table_name, ", ".join(kwargs.keys()), ", ".join(["'%s'" % kwargs[key] for key in kwargs.keys()]))
-    return send_command(self.connection, command)
-
-def get_data(self, *data):
-    """
-    Get data from a table.
-
-    Args:
-        table_name: Name of the table to get data from.
-        columns: List of columns to get data from. If not specified, all columns are used.
-    """
-    if len(data) == 1:
-        command = "SELECT * FROM %s" % data[0]
-    else:
-        command = "SELECT %s FROM %s" % (", ".join(data[1:]), data[0])
-    return send_command(self.connection, command)
-
-def get_table_creation_script(self, *args):
-    """
-    Returns the creation script for a table.
-
-    Args:
-        table_name: Name of the table to show the creation script for.
-    """
-    if len(args) == 0:
-        command = "SHOW CREATE TABLE %s" % self.current_table
-    else:
-        command = "SHOW CREATE TABLE %s" % table_name
-    return send_command(self.connection, command)
-
-def join_table(self, left: str, right: str, join_type: str, conditions: str):
-    """
-    Join two tables.
-
-    Args:
-        left: Name of the left table.
-        right: Name of the right table.
-        join_type: Type of join. Valid values are: INNER, LEFT, RIGHT, CROSS.
-        conditions: Conditions to join on.
-    """
-    command = "SELECT * FROM %s %s JOIN %s ON %s" % (left, join_type, right, conditions)
-    return send_command(self.connection, command)
-
-def get_foreign_keys(self, *args):
-    """
-    Get the foreign keys for a table.
-
-    Args:
-        table_name: Name of the table to get the foreign keys for.
-    """
-    if len(args) == 0:
-        command = "SELECT * FROM information_schema.key_column_usage WHERE REFERENCED_TABLE_NAME = '%s'" % self.current_table
-
-    command = "SELECT * FROM information_schema.KEY_COLUMN_USAGE WHERE TABLE_NAME = '%s' AND REFERENCED_TABLE_NAME IS NOT NULL" % args[0]
-    return send_command(self.connection, command)
-
 
--- Python/CLI/cli.py
+++ Python/CLI/cli.py
@@ -2,7 +2,7 @@
 
 import sys
 import argparse
-import  Database
+import  Database as db
 
 # A command line interface for accessing the APIs in the Database.py module.
 # The user is prompted to enter a command and the corresponding function is called.
@@ -20,7 +20,7 @@ args = parser.parse_args()
 # We create a connection to the database.
 
 
-link = Database.forge_connection(args.user, args.password, args.hostname, args.database)
+Database = db(args.user, args.password, args.hostname, args.database)
 
 # Still need to catch the exception if the connection fails.
 
@@ -44,14 +44,22 @@ def show_help(command: str):
     elif command == "status":
         print("status: Show the status of the database connection.")
     elif command == "show":
-        print("show: Shows the connents.")
+        print("show") # Show the available commands. Needs color.
         print("Usage: show [database]|[table]|[column]")
         print("show database: Displays the list of databases.")
         print("show table: Displays the list of tables of the current database.")
         print("show table <database>: Displays the list of tables of the specified database.")
         print("show column: Displays the list of columns of the current table.")
         print("show column <table>: Displays the list of columns of the given table.")
-
+        print("show column <database> <table>: Displays the list of columns of the given table in the given database.")
+    elif command == "add": # Add a new table to the database.
+        print("Usage: add <database> | <table> | <column>")
+        print("add database <name>: Adds a new database.")
+        print("add table <name> <columns>: Adds a new table to the current database.")
+        print("add table <name> <database> <columns>: Adds a new table to the specified database.") #Need to think what if the database doesn't exist.
+        print("add column <name> <description>: Adds a new column to the current table.")
+        print("add column <name> <description> <table>: Adds a new column to the given table.")
+        print("add column <name> <description> <database> <table>: Adds a new column to the given table in the given database.")
 
 
 def parse_command(response: str):
@@ -66,21 +74,42 @@ def parse_command(response: str):
         status()
     elif command[0] == "show":
         if command[1] == "database":
-            Database.show_databases(link)
+            Database.show_databases()
         elif command[1] == "table":
             try:
-                Database.show_tables(link, command[2])
+                Database.show_tables(command[2])
             except IndexError:
-                Database.show_tables(link)
+                Database.show_tables()
         elif command[1] == "column":
             try:
-                Database.show_columns(link, command[2])
+                Database.show_columns(".".join(command[1:3]))
             except IndexError:
-                Database.show_columns(link)
+                try:
+                    Database.show_columns(command[2])
+                except IndexError:
+                    Database.show_columns()
         else:
             print("Invalid usage of show. See help for more information.")
+    elif command[0] == "add":
+        if command[1] == "database":
+            Database.add_database(command[2])
+        elif command[1] == "table":
+            try:
+                Database.create_table(".".join(command[1:3]), command[4])
+            except IndexError:
+                Database.create_table(command[2], command[3])
+        elif command[1] == "column":
+            try:
+                Database.add_column(command[2], command[3], ".".join(command[3:5]))
+            except IndexError:
+                try:
+                    Database.add_column(command[2], command[3], command[4])
+                except IndexError:
+                    Database.add_column(command[2], command[3])
+        else:
+            print("Invalid usage of add. See help for more information.")
     else:
-        print("Unknown command: ", command)
+        print("Unknown command: " + command[0])
     return 
 
 while (True):
